85f432b4d77008187681f5085e3c33b6be02526a
Gabriel campos <gabrielfigueira88@gmail.com>
2026-02-12 22:36:07 -0300

    começando o site da barbearia

---
+++ barbearia/wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿    window.addEventListener("scroll", function () {
+        const heroBg = document.querySelector(".hero-section::before");
+    });
+
+    function toggleServices(id) {
+
+       
+        document.querySelectorAll('.services-box').forEach(box => {
+            box.style.display = "none";
+        });
+
+    document.getElementById(id).style.display = "block";
+    }
+
